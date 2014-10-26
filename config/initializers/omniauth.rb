eb06f9e92c55dae709ffb522cb5314a59a6fe9be
Mike Stenzler <mike@nexuscafe.com>
2014-10-26 17:57:30 -0400

    Move meetup keys from application.yml to secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :meetup, CONFIG[:meetup_key], CONFIG[:meetup_secret]
+	provider :meetup, Rails.application.secrets.meetup_key, Rails.application.secrets.meetup_secret
 #	provider :facebook, CONFIG[:facebook_id], ENV[:facebook_secret]
 #  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 #  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
